0f56a0cbaeda9020a2ecd591a0402fd9699d5d6b
Rohith Uppala <rohithfortopcoder@gmail.com>
2020-06-03 16:43:29 +0200

    Initial implementation of tcp listener and sender between golang and python
    This is needed to integrate MTCNN model into our service. Until we find
    a native alternative, we have to rely on network call

---
+++ external/sender.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+const (
+	ConnHost = "localhost"
+	ConnPort = "3333"
+	ConnType = "tcp"
+)
+
+func main() {
+	l, _ := net.Listen(ConnType, ConnHost+":"+ConnPort)
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("error: " + err.Error())
+			os.Exit(1)
+		}
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("error: " + err.Error())
+	}
+	fmt.Println(string(buffer))
+	conn.Write([]byte("MEssage recevied"))
+	conn.Close()
+}
